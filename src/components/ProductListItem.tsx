0468a70ac9ca605b9ee3a232b57f0a8cc28777a1
asrayg <asraygopa@gmail.com>
2024-06-29 20:19:44 -0500

    Admin Side

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,14 +1,16 @@
 import Colors from '../constants/Colors';
 import { StyleSheet, Text, Image, Pressable } from 'react-native';
 import { Product } from '../types';
-import {Link} from 'expo-router';
+import {Link, useSegments} from 'expo-router';
 
 type ProductListItem = {
     product: Product
 }
 const ProductListItem = ({product}: ProductListItem) =>{
+  const segments = useSegments()
+  console.log(segments);
   return (
-    <Link href = {`/menu/${product.id}`} asChild>
+    <Link href = {`/${segments[0]}/menu/${product.id}`} asChild>
     <Pressable  style={styles.container}>
     <Image source={{uri: product.image || 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png' }} 
     style={styles.image}
